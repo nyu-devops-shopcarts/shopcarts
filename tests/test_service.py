5b14893ca07ba03a1f629a909f8a315cc893ef5e
Neil Vijapura <ndv248@stern.nyu.edu>
2020-03-28 18:51:49 +0000

    Adding function to List items within a ShopCart

--- tests/test_service.py
+++ tests/test_service.py
@@ -178,3 +178,36 @@ class TestYourResourceServer(TestCase):
         self.assertEqual(data["sku"], item.sku)
         self.assertEqual(data["quantity"], item.quantity)
         self.assertEqual(data["price"], item.price)
+
+##### Listing Test Case ## 
+    def test_get_shopcart_items_list(self):
+        """ Get a list of Items in a ShopCart """
+        # add two addresses to account
+        shopcart = self._create_shopcarts(1)[0]
+        item_list = CartItemFactory.create_batch(2)
+
+        # Create item 1
+        resp = self.app.post(
+            "/shopcarts/{}/items".format(shopcart.id), 
+            json=item_list[0].serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # Create item 2
+        resp = self.app.post(
+            "/shopcarts/{}/items".format(shopcart.id), 
+            json=item_list[1].serialize(), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # get the list back and make sure there are 2
+        resp = self.app.get(
+            "/shopcarts/{}/items".format(shopcart.id), 
+            content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+        data = resp.get_json()
+        self.assertEqual(len(data), 2)
